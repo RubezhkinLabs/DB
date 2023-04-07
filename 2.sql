52f302136258c41ecfbd2056b430ed8f538b3b49
Pavel Nedobezhkin <96119105+Rubezhkin@users.noreply.github.com>
2023-04-07 16:42:14 +0300

    Add files via upload

--- 2.sql
+++ 2.sql
@@ -1,19 +1,52 @@
-select last_name, job_id, hire_date as "STARDATE", employee_id from employees;
+select last_name, job_id, hire_date, employee_id from employees
+where salary > 12000;
 
-select distinct job_id from employees;
+select last_name, department_id from employees
+where employee_id = 176;
 
-select last_name as "Employee", employee_id as "Emp #",hire_date as "Hire Date", job_id as "Job" from employees;
+select last_name, first_name, salary, department_id from employees
+where salary between 5000 and 12000;
 
-select last_name || ', ' || job_id as "Employee and Title" from employees;
+select last_name, job_id, hire_date from employees
+where last_name in ('Matos','Taylor');
 
-select last_name as "Employee", salary, salary*1.15 as "Next Year Salary" from employees;
+select last_name, department_id from employees
+where department_id in (20,50);
 
-select last_name, salary*12 from employees;
- 
-select last_name || ' for the year received ' || salary *12 as "Total salary", salary*(1+coalesce(commission_pct,0)) as "Premium Salary" from employees;
+select last_name || ' ' ||first_name as "Employee", salary as "Monthly Salary", department_id from employees
+where salary between 5000 and 12000;
 
-select last_name|| E'\'s salary is '|| salary from employees;
+select last_name, employee_id from employees
+where extract(year from hire_date)=2002;
 
-select first_name || last_name || 'has phone number ' || phone_number from employees;
+select last_name, employee_id from employees
+where manager_id is null;
 
-select distinct job_id from employees;
+select first_name, last_name, salary,commission_pct from employees
+where commission_pct is not null
+order by salary desc, salary*commission_pct desc;
+
+select first_name, last_name, salary from employees
+where salary > 10000;
+
+select employee_id,first_name, last_name, salary,department_id from employees
+where manager_id in (103,201,124)
+order by last_name;
+
+select employee_id, first_name, last_name from employees
+where last_name like '__a%';
+
+select employee_id, first_name, last_name from employees
+where last_name like '%a%' and last_name like '%e%';
+
+select employee_id, first_name, last_name, salary from employees
+where job_id in ('SA_REP', 'ST_CLERK') and salary not in(2500,3500,7000) ;
+
+select employee_id, first_name, last_name, salary from employees
+where commission_pct = 0.2;
+
+select last_name, job_id, salary,commission_pct, salary*(1+commission_pct) from employees
+where commission_pct is not null;
+
+select last_name, job_id from employees
+where job_id like 'SA!_%' ESCAPE '!'
