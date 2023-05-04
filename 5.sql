8f309532b71403a6b33bfc1ba3a5f41e848489e2
Pavel Nedobezhkin <96119105+Rubezhkin@users.noreply.github.com>
2023-05-04 17:16:15 +0300

    Update 5.sql

--- 5.sql
+++ 5.sql
@@ -24,3 +24,38 @@ from departments d
 inner join employees e
 on(d.manager_id = e.employee_id);
 
+select d.department_id, d.department_name, man.last_name, e.employee_id, e.last_name
+from departments d
+inner join employees e
+on(d.manager_id = e.manager_id)
+inner join employees man
+on(d.manager_id = man.employee_id);
+
+select e.employee_id, e.last_name, e.hire_date, d.hire_date
+from employees d
+join employees e
+on(e.hire_date > d.hire_date and d.last_name = 'Davies');
+
+select e.employee_id, e.last_name, e.hire_date, man.last_name, man.hire_date
+from employees man
+join employees e
+on(e.hire_date < man.hire_date and e.manager_id = man.employee_id);
+
+select department_id, department_name, count(employee_id)
+from employees
+natural join departments
+group by department_id, department_name;
+
+select e.employee_id, count(h.job_id) as job_count
+from employees e
+join job_history h
+on(e.employee_id = h.employee_id)
+group by e.employee_id having count(h.job_id) != 1;
+
+select e.employee_id, e.last_name, man.employee_id, man.last_name
+from employees man
+join employees e
+on(e.salary > man.salary and e.manager_id = man.employee_id);
+
+
+
